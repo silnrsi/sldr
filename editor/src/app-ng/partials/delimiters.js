cfe73523eb27d7df9722e6b832bc665f75eb4973
Martin Hosken <martin_hosken@sil.org>
2014-10-31 16:03:41 +0700

    Cover all of delimiters

--- editor/src/app-ng/partials/delimiters.js
+++ editor/src/app-ng/partials/delimiters.js
@@ -7,6 +7,8 @@ angular.module('ldmlEdit.delimiters', [
 
     $scope.vm = {};
     $scope.vm.quotes = [ {}, {} ];
+    $scope.vm.paraContType = null;
+    $scope.vm.paraContMark = null;
     var quotemap = {
         'quotationStart' : [0, 'start'],
         'quotationEnd' : [0, 'end'],
@@ -21,7 +23,8 @@ angular.module('ldmlEdit.delimiters', [
     var init = function(e) {
         var curr;
         $scope.fres = DomService.findElement(null, "delimiters");
-        if ($scope.fres == null) return;
+        if ($scope.fres == null)
+            $scope.fres = {tag : 'delimiters', attributes : {}, children : []};
         for (var k in quotemap) {
             curr = DomService.findElement($scope.fres, k);
             if (curr != null)
@@ -29,6 +32,11 @@ angular.module('ldmlEdit.delimiters', [
         }
         var spec = DomService.findElements($scope.fres, ["special", "sil:quotation-marks"]);
         if (spec != null) {
+            if ('paraContinueType' in spec.attributes)
+                $scope.vm.paraContType = spec.attributes['paraContinueType'];
+            if ('paraContinueMark' in spec.attributes)
+                $scope.vm.paraContMark = spec.attributes['paraContinueMark'];
+
             for (var k in silquotemap) {
                 curr = DomService.findElement(spec, k);
                 if (curr != null)
@@ -45,4 +53,37 @@ angular.module('ldmlEdit.delimiters', [
     };
     $scope.$on('dom', init);
     init();
+
+    var update_model = function() {
+        $scope.fres.children = [];
+        for (var k in quotemap)
+            $scope.fres.children.push({tag : k, attributes : {}, text : $scope.vm.quotes[quotemap[k][0]][quotemap[k][1]] });
+        if ($scope.vm.quotes.length > 2 || ($scope.vm.paraContType != null && $scope.vm.paraContType != 'None') || $scope.vm.paraContMark != null) {
+            var spec = {tag : 'sil:quotation-marks', attributes : {}, children : []};
+            if ($scope.vm.paraContType != null && $scope.vm.paraContType != 'None')
+                spec.attributes.paraContinueType = $scope.vm.paraContType;
+            if ($scope.vm.paraContMark != null)
+                spec.attributes.paraContinueMark = $scope.vm.paraContMark;
+            for (var i = 2; i < $scope.vm.quotes.length; i++) {
+                var s = $scope.vm.quotes[i];
+                spec.children.push({tag : 'sil:quotation', attributes : { open : s.start, close : s.end, 'continue' : s['continue'] } });
+            }
+            $scope.fres.children.push({tag : 'special', attributes : {}, children : [ spec ]});
+        }
+        DomService.updateTopLevel($scope.fres);
+    };
+            
+    $scope.addQuote = function() {
+        $scope.vm.quotes.push({start : '', end : '', 'continue' : ''});
+    };
+    $scope.delQuote = function(ind) {
+        $scope.vm.quotes.splice(ind, 1);
+    };
+    $scope.saveBtn = function() {
+        update_model();
+    };
+    $scope.cancelBtn = function() {
+        init();
+    };
+
 }]);
