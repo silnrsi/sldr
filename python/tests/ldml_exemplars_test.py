605227cc8f307c068258fbc21acdb11ea219bc1d
Bobby de Vos <bobby_devos@sil.org>
2018-02-15 17:00:48 -0700

    Redo tests since the callee will handle Unicode Set format
    This choice (caller versus callee) was confirmed late last month.

--- python/tests/ldml_exemplars_test.py
+++ python/tests/ldml_exemplars_test.py
@@ -32,7 +32,7 @@ try:
     from sldr.ldml_exemplars import UCD, ExemplarsItemString
 except ImportError:
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib')))
-    from sldr.ldml_exemplars import UCD, ExemplarsItemString
+    from sldr.ldml_exemplars import UCD, Exemplars
 
 
 class UCDTests(unittest.TestCase):
@@ -163,7 +163,7 @@ class UCDTests(unittest.TestCase):
 class ExemplarsTests(unittest.TestCase):
 
     def setUp(self):
-        self.exemplars = ExemplarsItemString()
+        self.exemplars = Exemplars()
 
     def tearDown(self):
         pass
@@ -171,91 +171,91 @@ class ExemplarsTests(unittest.TestCase):
     def test_simple_main(self):
         self.exemplars.process(u'[{cab.1}]')
         self.exemplars.analyze()
-        self.assertEqual(u'a b c', self.exemplars.main)
-        self.assertEqual(u'1', self.exemplars.digits)
+        self.assertEqual(u'[a b c]', self.exemplars.main)
+        self.assertEqual(u'[1]', self.exemplars.digits)
 
     def test_simple_punctuation(self):
         self.exemplars.process(u'[{cab.1}]')
         self.exemplars.analyze()
-        self.assertEqual(u'. [ ] { }', self.exemplars.punctuation)
-        self.assertEqual(u'1', self.exemplars.digits)
+        self.assertEqual(u'[. [ ] { }]', self.exemplars.punctuation)
+        self.assertEqual(u'[1]', self.exemplars.digits)
 
     def test_japanese_katakana(self):
         """Characters with Word_Break property Katakana are letters."""
         self.exemplars.process(u'\u307b\u309b')
         self.exemplars.analyze()
-        self.assertEqual(u'\u307b \u309b', self.exemplars.main)
+        self.assertEqual(u'[\u307b \u309b]', self.exemplars.main)
 
     def test_hebrew_aletter(self):
         """Characters with Word_Break property ALetter are not punctuation."""
         self.exemplars.process(u'\u05d1\u05f3\u05d2')
         self.exemplars.analyze()
-        self.assertEqual(u'\u05d1 \u05d2 \u05f3', self.exemplars.main)
-        self.assertEqual(u'', self.exemplars.punctuation)
+        self.assertEqual(u'[\u05d1 \u05d2 \u05f3]', self.exemplars.main)
+        self.assertEqual(u'[]', self.exemplars.punctuation)
 
     def test_hebrew_midletter(self):
         """Characters with Word_Break property MidLetter are not punctuation."""
         self.exemplars.process(u'\u05f4\u05d0\u05f4')
         self.exemplars.analyze()
-        self.assertEqual(u'\u05d0 \u05f4', self.exemplars.main)
-        self.assertEqual(u'', self.exemplars.punctuation)
+        self.assertEqual(u'[\u05d0 \u05f4]', self.exemplars.main)
+        self.assertEqual(u'[]', self.exemplars.punctuation)
 
     def test_chinese(self):
         self.exemplars.process(u'\u6606\u660e\ua700')
         self.exemplars.analyze()
-        self.assertEqual(u'\u6606 \u660e', self.exemplars.main)
+        self.assertEqual(u'[\u6606 \u660e]', self.exemplars.main)
 
     def test_png(self):
         """Digits are ignored, unless they have diacritics."""
         self.exemplars.process(u'1\u0301 2\u0301 3\u0301 4\u0301 5\u0301 6\u0301')
         self.exemplars.analyze()
-        self.assertEqual(u'1 2 3 4 5 6 \u0301', self.exemplars.main)
-        self.assertEqual(u'', self.exemplars.digits)
+        self.assertEqual(u'[1 2 3 4 5 6 \u0301]', self.exemplars.main)
+        self.assertEqual(u'[]', self.exemplars.digits)
 
     def test_not_included(self):
         self.exemplars.process(u'\u034f\u00ad\u06dd')
         self.exemplars.analyze()
-        self.assertEqual(u'', self.exemplars.main)
+        self.assertEqual(u'[]', self.exemplars.main)
 
     def test_lithuanian_main(self):
         self.exemplars.process(u'\u00c1\u0328 \u00e1\u0328 I\u0307\u0301 i\u0307\u0301')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0105 i\u0307 \u0301', self.exemplars.main)
+        self.assertEqual(u'[\u0105 {i\u0307} \u0301]', self.exemplars.main)
 
     def test_lithuanian_index(self):
         self.exemplars.process(u'a \u0105 b c A \u0104 B C Z')
         self.exemplars.analyze()
-        self.assertEqual(u'A \u0104 B C', self.exemplars.index)
+        self.assertEqual(u'[A \u0104 B C]', self.exemplars.index)
 
     def test_english_main(self):
-        self.exemplars.auxiliary = u'\u00e9'
+        self.exemplars.auxiliary = u'[\u00e9]'
         self.exemplars.process(u'r\u00e9sum\u00e9')
         self.exemplars.analyze()
-        self.assertEqual(u'm r s u', self.exemplars.main)
+        self.assertEqual(u'[m r s u]', self.exemplars.main)
 
     def test_english_auxiliary_nfc(self):
-        self.exemplars.auxiliary = u'\u00e9'
+        self.exemplars.auxiliary = u'[\u00e9]'
         self.exemplars.process(u'r\u00e9sum\u00e9')
         self.exemplars.analyze()
-        self.assertEqual(u'\u00e9', self.exemplars.auxiliary)
+        self.assertEqual(u'[\u00e9]', self.exemplars.auxiliary)
 
     def test_english_auxiliary_nfd(self):
-        self.exemplars.auxiliary = u'e\u0301'
+        self.exemplars.auxiliary = u'[{e\u0301}]'
         self.exemplars.process(u're\u0301sume\u0301')
         self.exemplars.analyze()
-        self.assertEqual(u'\u00e9', self.exemplars.auxiliary)
+        self.assertEqual(u'[\u00e9]', self.exemplars.auxiliary)
 
     def test_english_index(self):
-        self.exemplars.auxiliary = u'\u00e9'
+        self.exemplars.auxiliary = u'[\u00e9]'
         self.exemplars.process(u'r\u00e9sum\u00e9')
         self.exemplars.analyze()
-        self.assertEqual(u'\u00c9 M R S U', self.exemplars.index)
+        self.assertEqual(u'[\u00c9 M R S U]', self.exemplars.index)
 
     def test_spanish(self):
         """Marks occurring on a few bases are not separate."""
         self.exemplars.process(u'biling\u00fce')
         self.exemplars.analyze()
-        self.assertEqual(u'b e g i l n \u00fc', self.exemplars.main)
+        self.assertEqual(u'[b e g i l n \u00fc]', self.exemplars.main)
 
     def test_french_main_nfc(self):
         """Marks occurring on many bases are separate.
@@ -265,19 +265,19 @@ class ExemplarsTests(unittest.TestCase):
         self.exemplars.many_bases = 4
         self.exemplars.process(u'r\u00e9sum\u00e9 \u00e2 \u00ea \u00ee \u00f4 \u00fb')
         self.exemplars.analyze()
-        self.assertEqual(u'a e \u00e9 i m o r s u \u0302', self.exemplars.main)
+        self.assertEqual(u'[a e \u00e9 i m o r s u \u0302]', self.exemplars.main)
 
     def test_french_main_nfd(self):
         """Marks occurring on many bases are separate."""
         self.exemplars.many_bases = 4
         self.exemplars.process(u're\u0301sume\u0301 a\u0302 e\u0302 i\u0302 o\u0302 u\u0302')
         self.exemplars.analyze()
-        self.assertEqual(u'a e \u00e9 i m o r s u \u0302', self.exemplars.main)
+        self.assertEqual(u'[a e \u00e9 i m o r s u \u0302]', self.exemplars.main)
 
     def test_french_auxiliary(self):
         self.exemplars.process(u'r\u00e9sum\u00e9')
         self.exemplars.analyze()
-        self.assertEqual(u'', self.exemplars.auxiliary)
+        self.assertEqual(u'[]', self.exemplars.auxiliary)
 
     def test_french_count(self):
         """Infrequently occurring exemplars should go in the auxiliary list, not the main list."""
@@ -288,25 +288,25 @@ class ExemplarsTests(unittest.TestCase):
         circumflex = u'\u00e2 \u00ea \u00ee \u00f4 \u00fb'
         self.exemplars.process(base + grave + circumflex)
         self.exemplars.analyze()
-        self.assertEqual(u'a e i o u \u0302', self.exemplars.main)
-        self.assertEqual(u'\u00e0 \u00e8 \u00f9', self.exemplars.auxiliary)
+        self.assertEqual(u'[a e i o u \u0302]', self.exemplars.main)
+        self.assertEqual(u'[\u00e0 \u00e8 \u00f9]', self.exemplars.auxiliary)
 
     def test_french_index(self):
         self.exemplars.process(u'r\u00e9sum\u00e9')
         self.exemplars.analyze()
-        self.assertEqual(u'\u00c9 M R S U', self.exemplars.index)
+        self.assertEqual(u'[\u00c9 M R S U]', self.exemplars.index)
 
     def test_swahil_main(self):
-        self.exemplars.main = u'ng ng\ua78c'
+        self.exemplars.main = u'[{ng} {ng\ua78c}]'
         self.exemplars.process(u'ran rang rang\ua78c')
         self.exemplars.analyze()
-        self.assertEqual(u'a n ng ng\ua78c r', self.exemplars.main)
+        self.assertEqual(u'[a n {ng} {ng\ua78c} r]', self.exemplars.main)
 
     def test_swahili_index(self):
-        self.exemplars.main = u'ng ng\ua78c'
+        self.exemplars.main = u'[{ng} {ng\ua78c}]'
         self.exemplars.process(u'ran rang rang\ua78c')
         self.exemplars.analyze()
-        self.assertEqual(u'A N NG NG\ua78b R', self.exemplars.index)
+        self.assertEqual(u'[A N {NG} {NG\ua78b} R]', self.exemplars.index)
 
     def test_swahili_glottal(self):
         """Exemplars have a specific script, unless they have specific Word_Break properties.
@@ -317,16 +317,16 @@ class ExemplarsTests(unittest.TestCase):
         """
         self.exemplars.process(u'ng\ua78c ng\u02bc ng\u02c0')
         self.exemplars.analyze()
-        self.assertEqual(u'g n \u02bc \u02c0 \ua78c', self.exemplars.main)
+        self.assertEqual(u'[g n \u02bc \u02c0 \ua78c]', self.exemplars.main)
 
     def test_devanagari_many(self):
         """Indic matras are always separate."""
         self.exemplars.process(u'\u0958\u093e \u0959\u093e \u095a\u093e \u095b\u093e '
                                u'\u095c\u093e \u095d\u093e \u095e\u093e \u095f\u093e')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0915\u093c \u0916\u093c \u0917\u093c \u091c\u093c '
-                         u'\u0921\u093c \u0922\u093c \u092b\u093c \u092f\u093c '
-                         u'\u093e',
+        self.assertEqual(u'[{\u0915\u093c} {\u0916\u093c} {\u0917\u093c} {\u091c\u093c}'
+                         u' {\u0921\u093c} {\u0922\u093c} {\u092b\u093c} {\u092f\u093c}'
+                         u' \u093e]',
                          self.exemplars.main)
 
     def test_devanagari_few(self):
@@ -338,14 +338,14 @@ class ExemplarsTests(unittest.TestCase):
         """
         self.exemplars.process(u'\u0958\u093e \u0959\u093e \u095a\u093e')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0915\u093c \u0916\u093c \u0917\u093c \u093e',
+        self.assertEqual(u'[{\u0915\u093c} {\u0916\u093c} {\u0917\u093c} \u093e]',
                          self.exemplars.main)
 
     def test_devanagari_graphemes(self):
         """Graphemes are the found clusters before doing analysis."""
         self.exemplars.process(u'\u0958\u093e \u0959\u093e \u095a\u093e')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0917\u093c\u093e \u0916\u093c\u093e \u0915\u093c\u093e',
+        self.assertEqual(u'[{\u0917\u093c\u093e} {\u0916\u093c\u093e} {\u0915\u093c\u093e}]',
                          self.exemplars.graphemes)
 
     def test_devanagari_index(self):
@@ -354,7 +354,7 @@ class ExemplarsTests(unittest.TestCase):
                                u'\u0915\u093e \u0916\u093e '
                                u'\u0958\u093e \u0959\u093e')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0905 \u0906 \u0915 \u0915\u093c \u0916 \u0916\u093c',
+        self.assertEqual(u'[\u0905 \u0906 \u0915 {\u0915\u093c} \u0916 {\u0916\u093c}]',
                          self.exemplars.index)
 
     def test_devanagari_vedic(self):
@@ -364,31 +364,31 @@ class ExemplarsTests(unittest.TestCase):
         """
         self.exemplars.process(u'\u0915\u1cd1')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0915 \u1cd1', self.exemplars.main)
+        self.assertEqual(u'[\u0915 \u1cd1]', self.exemplars.main)
 
     def test_kannada_main_old(self):
         """Clusters with virama, ZWJ."""
         self.exemplars.process(u'\u0cb0\u0ccd\u200d\u0c95 \u0c95\u0ccd\u200d\u0c95')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0c95 \u0cb0 \u0ccd', self.exemplars.main)
+        self.assertEqual(u'[\u0c95 \u0cb0 \u0ccd]', self.exemplars.main)
 
     def test_kannada_main_new(self):
         """Clusters with ZWJ, virama."""
         self.exemplars.process(u'\u0cb0\u200d\u0ccd\u0c95 \u0c95\u200d\u0ccd\u0c95')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0c95 \u0cb0 \u0ccd', self.exemplars.main)
+        self.assertEqual(u'[\u0c95 \u0cb0 \u0ccd]', self.exemplars.main)
 
     def test_kannada_auxiliary(self):
         """A Default_Ignorable_Code_Point such as ZWJ goes into the auxiliary exemplar."""
         self.exemplars.process(u'\u0cb0\u200d\u0ccd\u0c95')
         self.exemplars.analyze()
-        self.assertEqual(u'\u200d', self.exemplars.auxiliary)
+        self.assertEqual(u'[\u200d]', self.exemplars.auxiliary)
 
     def test_kannada_graphemes(self):
         """Clusters are useful for testing rendering."""
         self.exemplars.process(u'\u0cb0\u200d\u0ccd\u0c95 \u0cb0\u0ccd\u200d\u0c95')
         self.exemplars.analyze()
-        self.assertEqual(u'\u0c95 \u0cb0\u200d\u0ccd \u0cb0\u0ccd\u200d', self.exemplars.graphemes)
+        self.assertEqual(u'[\u0c95 {\u0cb0\u200d\u0ccd} {\u0cb0\u0ccd\u200d}]', self.exemplars.graphemes)
 
     def test_kannada_script(self):
         """Find most frequently occurring script."""
@@ -416,13 +416,13 @@ class ExemplarsTests(unittest.TestCase):
                                u'o\u0301 o\u0300 o\u0304 '
                                u'u\u0301 u\u0300 u\u0304 ')
         self.exemplars.analyze()
-        self.assertEqual(u'a e i o u \u0300 \u0301 \u0304', self.exemplars.main)
+        self.assertEqual(u'[a e i o u \u0300 \u0301 \u0304]', self.exemplars.main)
 
     def test_vietnamese_vowels(self):
         """Some diacritics indicate additional vowels."""
         self.exemplars.process(u'\u0103\u00e2\u0111\u00ea\u00f4\u01a1\u01b0')
         self.exemplars.analyze()
-        self.assertEqual(u'\u00e2 \u0103 \u00ea \u00f4 \u01a1 \u01b0 \u0111', self.exemplars.main)
+        self.assertEqual(u'[\u00e2 \u0103 \u00ea \u00f4 \u01a1 \u01b0 \u0111]', self.exemplars.main)
 
     def test_vietnamese_tones(self):
         """Other diacritics indicate tones."""
@@ -435,15 +435,15 @@ class ExemplarsTests(unittest.TestCase):
         text = mid + low_falling + mid_falling + glottal_rising + high_rising + glottal_falling
         self.exemplars.process(text)
         self.exemplars.analyze()
-        self.assertEqual(u'a \u00e2 \u0103 e \u00ea i o \u00f4 \u01a1 u \u01b0 y '
-                         u'\u0300 \u0301 \u0303 \u0309 \u0323', self.exemplars.main)
+        self.assertEqual(u'[a \u00e2 \u0103 e \u00ea i o \u00f4 \u01a1 u \u01b0 y'
+                         u' \u0300 \u0301 \u0303 \u0309 \u0323]', self.exemplars.main)
 
     def test_stacking_diacritics(self):
         """Second level diacritics are always separate."""
         self.exemplars.process(u'a\u0324 b\u032a c\u0303 d\u0306 e\u0301 '
                                u'f\u0324\u032a\u0303\u0306\u0301')
         self.exemplars.analyze()
-        self.assertEqual(u'a\u0324 b c\u0303 d e f\u0324\u0303 \u0301 \u0306 \u032a',
+        self.assertEqual(u'[{a\u0324} b {c\u0303} d e {f\u0324\u0303} \u0301 \u0306 \u032a]',
                          self.exemplars.main)
 
 
