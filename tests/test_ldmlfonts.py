107e7fde16f3521b9993b18ec66a7f128882dde9
Bobby de Vos <bobby_devos@sil.org>
2020-01-22 17:05:46 -0700

    Read langtags.csv and initial pass to write to LDML files

--- tests/test_ldmlfonts.py
+++ tests/test_ldmlfonts.py
@@ -10,21 +10,25 @@ except ImportError:
     from wstools import ldmlfonts
 
 
-def test_arabic_africa():
+def setup_module():
     ldmlfonts.read_font_data('Script2Font.csv')
+
+
+def test_arabic_africa():
     assert ldmlfonts.suggestions['Arab_NG'][0] == 'Harmattan'
 
 
 def test_arabic_asia():
-    ldmlfonts.read_font_data('Script2Font.csv')
     assert ldmlfonts.suggestions['Arab_PK'][0] == 'Awami Nastaliq'
 
 
 def test_arabic():
-    ldmlfonts.read_font_data('Script2Font.csv')
     assert ldmlfonts.suggestions['Arab'][0] == 'Scheherazade'
 
 
 def test_coptic():
-    ldmlfonts.read_font_data('Script2Font.csv')
     assert ldmlfonts.suggestions['Copt'][1] == 'Sophia Nubian'
+
+
+def test_greek():
+    assert ldmlfonts.suggestions['Grek'][1] == 'Galatia'
