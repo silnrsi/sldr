b576d53d7a3aa42654cc1b49a5e3365d28872d77
Bobby de Vos <bobby_devos@sil.org>
2020-01-27 09:24:18 -0700

    Test cases for selecting a font

--- tests/test_ldmlfonts.py
+++ tests/test_ldmlfonts.py
@@ -7,7 +7,7 @@ try:
     from bin import ldmlfonts
 except ImportError:
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'bin')))
-    from wstools import ldmlfonts
+    from bin import ldmlfonts
 
 
 def setup_module():
@@ -30,5 +30,41 @@ def test_coptic():
     assert ldmlfonts.suggestions['Copt'][1] == 'Sophia Nubian'
 
 
-def test_greek():
-    assert ldmlfonts.suggestions['Grek'][1] == 'Galatia'
+def test_aa():
+    assert ldmlfonts.find_fonts('aa')[0] == 'Charis SIL'
+
+
+def test_aa_arab():
+    assert ldmlfonts.find_fonts('aa-Arab')[0] == 'Scheherazade'
+
+
+def test_cop():
+    assert ldmlfonts.find_fonts('cop')[0] == 'NotoSansCoptic'
+
+
+def test_cop_arab():
+    assert ldmlfonts.find_fonts('cop-Arab')[0] == 'Scheherazade'
+
+
+def test_cop_grek():
+    assert ldmlfonts.find_fonts('cop-Grek')[0] == 'Gentium Plus'
+
+
+def test_el():
+    assert ldmlfonts.find_fonts('el')[0] == 'Gentium Plus'
+
+
+def test_ha():
+    assert ldmlfonts.find_fonts('ha')[0] == 'Charis SIL'
+
+
+def test_ha_arab():
+    assert ldmlfonts.find_fonts('ha-Arab')[0] == 'Harmattan'
+
+
+def test_ha_cm():
+    assert ldmlfonts.find_fonts('ha-CM')[0] == 'Harmattan'
+
+
+def test_ha_sd():
+    assert ldmlfonts.find_fonts('ha-SD')[0] == 'Scheherazade'
