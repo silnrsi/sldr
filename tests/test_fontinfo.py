3f1703c22dae813df887beebe4a2806022e61d2f
Emily Roth <emily_roth@sil.org>
2023-05-11 12:00:40 -0400

    font test addition

--- tests/test_fontinfo.py
+++ tests/test_fontinfo.py
@@ -7,6 +7,7 @@
 #import sldr.UnicodeSets as usets
 from langtag import langtag, lookup
 import os
+from sldr.ldml import Ldml, _alldrafts
 
 def iscldr(ldml):
     i = ldml.ldml.root.find(".//identity/special/sil:identity", {v:k for k,v in ldml.ldml.namespaces.items()})
@@ -24,11 +25,75 @@ def isempty(ldml):
 
 def test_fontinfo(ldml, langid):
     """ Test that the LDML file has font information """
-    if iscldr(ldml):    # short circuit CLDR for now
-        return
+#    if iscldr(ldml):    # short circuit CLDR for now
+#        return
     if isempty(ldml):   #skips font test if file is only an identity block
         return
     filename = os.path.basename(ldml.ldml.fname)    # get filename for reference
     
+    def test_parents(langid):
+        lt = langtag(os.path.splitext(os.path.basename(langid))[0]) #gets basic langtag data based on file name
+        tagset = lookup(str(lt).replace("_", "-"), default="", matchRegions=True)
+        lt_text = str(tagset) #technically never used, but kept just in case need to compare old & new
+        root_tag = lt_text
+        root_tagset = tagset
+        
+        def _has_sldr(root_tagset):
+            return getattr(root_tagset, "sldr", None)
+        
+        print("pre-test")
+        print(root_tag)
+        print(root_tagset)
+        print(root_tagset.script)
+
+        def _remove_private(root_tag):
+            r = root_tag.rfind('-x')
+            if r > 0:
+                root_tag = root_tag[:r]
+            root_tagset = lookup(str(root_tag).replace("_", "-"), default="", matchRegions=False)
+            print("removed private:")
+            print(root_tag)
+            print(root_tagset)
+            return root_tag, root_tagset
+        ### i feel like this is unncessary except for the fact that the x will confuse things i think hmmm       
+
+        def _trim_tag(root_tag, root_tagset):
+            r = root_tag.rfind('-')
+            if r > 0:
+                root_tag_temp = root_tag[:r]
+                root_tagset_temp = lookup(str(root_tag_temp).replace("_", "-"), default="", matchRegions=False)
+                print("tag trim iteration:")
+                print(root_tag_temp)
+                print(root_tagset_temp)
+                print(root_tagset_temp.script)
+                if root_tagset_temp == root_tagset:
+                    print("tagsets still match, trim again!")
+                    _trim_tag(root_tag_temp, root_tagset)
+                elif root_tagset_temp.script == root_tagset.script:
+                    if _has_sldr(root_tagset_temp):
+                        print("new root found with sldr file :)")
+                        root_tag = root_tag_temp
+                    print("scripts still match, trim again!")
+                    _trim_tag(root_tag_temp, root_tagset)
+                elif root_tagset_temp == "":
+                    print("no tagset found that matches, idk how bc that's not how it works but this error is here anyway just in case")
+                    _trim_tag(root_tag_temp, root_tagset)
+                else:
+                    print("trimmed file has different script OR has no sldr file, final root tag is the last one we trimmed!")
+                    root_tagset = root_tagset_temp
+            else:
+                print("no more trimming, final root is the last one we trimmed!")
+            return root_tag, root_tagset            
+
+        root_tag, root_tagset = _remove_private(root_tag)
+        root_tag, root_tagset = _trim_tag(root_tag, root_tagset)
+        
+        if root_tagset == tagset:
+            print("this is the root tagset, proceed to test for font!")
+            return False
+        return True
+            
+    if test_parents(langid):
+        return    
     fonts = ldml.ldml.findall("special/sil:external-resources/sil:font")
     assert len(fonts) > 0 , filename + " has no font information"
