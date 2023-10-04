a0c77cceeaee4a6bfa181593c9f83757e6d20b82
Emily Roth <emily_roth@sil.org>
2023-10-04 11:24:20 -0400

    test core coverage

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -3,3 +3,27 @@ import requests
 import logging, os, re, unicodedata
 from langtag import langtag, lookup
 from sldr.utils import find_parents
+
+
+def iscldr(ldml):
+    i = ldml.ldml.root.find(".//identity/special/sil:identity", {v:k for k,v in ldml.ldml.namespaces.items()})
+    if i is not None and i.get('source', "") == "cldr":
+        return True
+    return False
+
+def test_core(ldml):
+    filename = os.path.basename(ldml.ldml.fname)    # get filename for reference
+    corereqs = {
+        "characters/exemplarCharacters": "Main Exemplar Characters", 
+        "characters/exemplarCharacters[@type='auxiliary']": "Auxiliary Exemplar Characters", 
+        "characters/exemplarCharacters[@type='punctuation']": "Punctuation Exemplar Characters",
+        "characters/exemplarCharacters[@type='numbers']": "Numbers Exemplar Characters",
+    }
+    coremissing = {}
+    for r in corereqs.keys():
+        req = ldml.ldml.root.find(r)
+        if req is None:
+            coremissing[r]=corereqs.get(r)
+    if len(coremissing) == 0: 
+        return
+    assert False, filename + " is missing Core Requirement(s): " + str(list(coremissing.values()))
